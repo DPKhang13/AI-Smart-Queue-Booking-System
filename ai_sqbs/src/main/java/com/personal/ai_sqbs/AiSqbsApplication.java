fe24383bf4d9c344bfe1ab72dfbbaf73395a159a
Đặng Phúc Khang <dangphuckhang10b2@gmail.com>
2026-05-13 17:49:44 +0700

    [REFACTOR] source code

---
+++ ai_sqbs/src/main/java/com/personal/ai_sqbs/AiSqbsApplication.java
@@ -0,0 +1,13 @@
+package com.personal.ai_sqbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSqbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSqbsApplication.class, args);
+    }
+
+}
---
+++ ai_sqbs/src/test/java/com/personal/ai_sqbs/AiSqbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.ai_sqbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSqbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
